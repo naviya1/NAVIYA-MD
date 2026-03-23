9dfb316c4b4aa6ac979def7edacdc07fc122960c
naviya1 <kaviqulaksan44@gmail.com>
2026-03-23 16:50:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ async function connectToWA() {
 ensureSessionFile();
 
 app.get("/", (req, res) => {
-  res.send("Hey, DANUWA-MD started✅");
+  res.send("Hey, NAVIYA-MD started✅");
 });
 
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
